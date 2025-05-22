d68544c2ecfe11779126c7fac5679d4bfd729e93
aarsh <aarshmail@gmail.com>
2025-05-22 10:31:58 +0530

    added jackson java 8 date/time support

--- src/main/java/com/merchant/api/config/RedisConfig.java
+++ src/main/java/com/merchant/api/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.merchant.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,18 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // Create ObjectMapper with JavaTimeModule for LocalDateTime support
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        
+        // Create serializer with configured ObjectMapper
+        GenericJackson2JsonRedisSerializer serializer = 
+            new GenericJackson2JsonRedisSerializer(objectMapper);
+        
         RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                        .fromSerializer(serializer));
         
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(cacheConfig)
